e78ffafedf05cb8fbc1a5809661e5367e6c18bfd
Tyler Tian <tylertian123@gmail.com>
2019-01-13 22:43:31 -0500

    Add basic service in bot_vision_node
    The enable_vision service enables or disables vision processing.
    It also sets the exposure of the main camera based on the parameters
    vision_exposure and normal_exposure.

--- src/bot_vision/src/bot_vision_node.cpp
+++ src/bot_vision/src/bot_vision_node.cpp
@@ -7,10 +7,54 @@
 #include <std_msgs/Int32.h>
 #include <std_srvs/SetBool.h>
 
-int main(int argc, char **argv) {
-	
-	ros::init(argc, argv, "bot_vision_node");
+// Keeps track of whether vision processing is on
+bool vision_on = false;
+// Publisher for exposure control
+ros::Publisher exposure_pub;
+// Vision and normal exposure values
+int vision_exposure, normal_exposure;
+// Callback for the service
+bool enable_vision_callback(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &resp) {
+	if(req.data) {
+		vision_on = true;
+		// Publish the exposure to set it
+		std_msgs::Int32 m;
+		m.data = vision_exposure;
+		exposure_pub.publish(m);
 
+		ROS_INFO_STREAM("Vision has been turned ON.");
+	}
+	else {
+		vision_on = false;
+		
+		std_msgs::Int32 m;
+		m.data = normal_exposure;
+		exposure_pub.publish(m);
+		
+		ROS_INFO_STREAM("Vision has been turned OFF.");
+	}
+	
+	resp.success = true;
+	return true;
+}
+
+int main(int argc, char **argv) {
+	// Init ROS
+	ros::init(argc, argv, "bot_vision_node");
+	// Get a reference to ths node
+	ros::NodeHandle node_handle("~");
+
+	// Init exposure publisher
+	exposure_pub = node_handle.advertise<std_msgs::Int32>("/main_camera/exposure", 5);
+	// Get the parameter values
+	node_handle.param("vision_exposure", vision_exposure, 5);
+	node_handle.param("normal_exposure", normal_exposure, 0);
+
+	// Advertise the service
+	ros::ServiceServer server = node_handle.advertiseService("enable_vision", enable_vision_callback);
+
+	// Run forever until shutdown
+	ros::spin();
 
 	return 0;
 }
