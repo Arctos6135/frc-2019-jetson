00ef385d1c05023da1766763462e504f8bba5862
Tyler Tian <tylertian123@gmail.com>
2019-01-15 23:00:57 -0500

    Set up angle result translator
    The result horizontal angle will be put into NetworkTables under the
    table "roborio-jetson" with key "horizontal-angle".

--- src/nt_ros_translate/src/nt_ros_translate_node.cpp
+++ src/nt_ros_translate/src/nt_ros_translate_node.cpp
@@ -1,15 +1,24 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
+#include <std_srvs/SetBool.h>
 #include <ntcore.h>
 #include <networktables/NetworkTable.h>
 
 #include <string>
+#include <memory>
 
+// NetworkTables IP address
 std::string nt_ip_addr;
+// Subscriber for receiving vision result
 ros::Subscriber result_sub;
-
+// Service client for turning vision on or off
+ros::ServiceClient vision_scli;
+// The shared table for roboRIO/Jetson comms
+std::shared_ptr<NetworkTable> table;
+// Callback for when a result is generated
 void result_callback(const std_msgs::Float64::ConstPtr &msg) {
-	// TODO
+	// Put the result into the table
+	table->PutNumber("horizontal-angle", msg->data);
 }
 
 int main(int argc, char **argv) {
@@ -26,9 +35,14 @@ int main(int argc, char **argv) {
 	NetworkTable::SetIPAddress(nt_ip_addr.c_str());
 	NetworkTable::Initialize();
 
+	table = NetworkTable::GetTable("roborio-jetson");
+
 	// Topic subscription
 	result_sub = node_handle.subscribe("bot_vision/result_horiz_angle", 1, result_callback);
 
+	// Service client setup
+	vision_scli = node_handle.serviceClient<std_srvs::SetBool>("/vision_processing_node/enable_vision");
+	
 	return 0;
 }
 
