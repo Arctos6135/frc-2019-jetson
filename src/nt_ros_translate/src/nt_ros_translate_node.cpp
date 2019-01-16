3901b38606ce2eb0ef3762fd772d1bb4e9846076
Tyler Tian <tylertian123@gmail.com>
2019-01-16 14:09:01 -0500

    Finish basic NetworkTables interface
    The table used to communicate is "roborio-jetson". The result angle is
    stored in "result-angle". The entry "vision-enable" is used to enable
    or disable vision processing, and the success of the operation is
    stored in "enable-success".

--- src/nt_ros_translate/src/nt_ros_translate_node.cpp
+++ src/nt_ros_translate/src/nt_ros_translate_node.cpp
@@ -1,8 +1,10 @@
 #include <ros/ros.h>
 #include <std_msgs/Float64.h>
 #include <std_srvs/SetBool.h>
+
 #include <ntcore.h>
 #include <networktables/NetworkTable.h>
+#include <tables/ITableListener.h>
 
 #include <string>
 #include <memory>
@@ -21,6 +23,25 @@ void result_callback(const std_msgs::Float64::ConstPtr &msg) {
 	table->PutNumber("horizontal-angle", msg->data);
 }
 
+// Table listener
+class VisionTableListener : public ITableListener {
+	void ValueChanged(ITable *source, llvm::StringRef key, std::shared_ptr<nt::Value> value, bool is_new) override {
+		if(key.compare("vision-enable") == 0 && value->IsBoolean()) {
+			std_srvs::SetBool args;
+			args.request.data = value->GetBoolean();
+			
+			if(vision_scli.call(args)) {
+				table->PutBoolean("enable-success", true);
+			}
+			else {
+				table->PutBoolean("enable-success", false);
+
+				ROS_FATAL_STREAM("Oh no! It's busted");
+			}
+		}
+	}
+};
+
 int main(int argc, char **argv) {
 	
 	ros::init(argc, argv, "nt_ros_translate_node");
@@ -30,6 +51,8 @@ int main(int argc, char **argv) {
 	// Retrieve parameters
 	node_handle.param<std::string>("nt_ip_addr", nt_ip_addr, "10.61.35.2");
 
+	ROS_INFO_STREAM("Connecting to NetworkTables with IP " << nt_ip_addr);
+
 	// NT setup
 	NetworkTable::SetClientMode();
 	NetworkTable::SetIPAddress(nt_ip_addr.c_str());
@@ -38,10 +61,18 @@ int main(int argc, char **argv) {
 	table = NetworkTable::GetTable("roborio-jetson");
 
 	// Topic subscription
-	result_sub = node_handle.subscribe("bot_vision/result_horiz_angle", 1, result_callback);
+	result_sub = node_handle.subscribe("/bot_vision/result_horiz_angle", 1, result_callback);
 
 	// Service client setup
 	vision_scli = node_handle.serviceClient<std_srvs::SetBool>("/vision_processing_node/enable_vision");
+
+	ros::Rate rate(25);
+
+	while(ros::ok()) {
+		ros::spinOnce();
+
+		rate.sleep();
+	}
 	
 	return 0;
 }
