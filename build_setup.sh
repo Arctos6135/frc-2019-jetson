5a889370d1ccc738f2c6e944ddb6a3e2b9d5ef76
Tyler Tian <tylertian123@gmail.com>
2019-02-16 16:02:52 -0500

    Add installation of ros-kinetic-web-video-server

--- build_setup.sh
+++ build_setup.sh
@@ -1,6 +1,7 @@
 catkin init
 cd src
 export ROS_DISTRO=kinetic
+sudo apt install ros-kinetic-web-video-server
 wstool update
 #rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
 #wstool update
