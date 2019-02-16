8ba1f62c1bdc33a419ce87ac2ea7e94d395f6404
Tyler Tian <tylertian123@gmail.com>
2019-02-16 10:58:14 -0500

    Modify rosdep command and add wstool update after it

--- build_setup.sh
+++ build_setup.sh
@@ -3,6 +3,7 @@ cd src
 wstool update
 #rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
 #wstool update
-rosdep install -y --from-paths . --ignore-src
+rosdep install -y --from-paths .
+wstool update
 source /opt/ros/kinetic/setup.bash
 
