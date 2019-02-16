c6c4862f48e53fdddfc3fa81f68c15bcafafc695
Tyler Tian <tylertian123@gmail.com>
2019-02-16 01:25:31 -0500

    Uncomment usb_cam setup

--- build_setup.sh
+++ build_setup.sh
@@ -1,7 +1,7 @@
 catkin init
 cd src
 wstool update
-#rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
-#wstool update
+rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
+wstool update
 source /opt/ros/kinetic/setup.bash
 
