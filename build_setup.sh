2925966347e29f309cc2a3d993346df7734aa5ee
Tyler Tian <tylertian123@gmail.com>
2019-02-16 18:56:53 -0500

    Add installation of ros-kinetic-web-video-server

--- build_setup.sh
+++ build_setup.sh
@@ -4,7 +4,7 @@ export ROS_DISTRO=kinetic
 wstool update
 #rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
 #wstool update
-sudo apt-get install ros-kinetic-camera-info-manager
+sudo apt install ros-kinetic-camera-info-manager ros-kinetic-web-video-server
 rosdep install -y --from-paths .
 wstool update
 source /opt/ros/kinetic/setup.bash
