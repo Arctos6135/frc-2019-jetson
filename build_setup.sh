e554b375a03ab34759889c848b90f1d39e45dbc5
Tyler Tian <tylertian123@gmail.com>
2019-02-16 10:42:17 -0500

    Remove unnecessary command and add usb_cam deps install

--- build_setup.sh
+++ build_setup.sh
@@ -1,7 +1,8 @@
 catkin init
 cd src
 wstool update
-rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
-wstool update
+#rosinstall_generator usb_cam --rosdistro kinetic | wstool merge -
+#wstool update
+rosdep install --from-paths . --ignore-src
 source /opt/ros/kinetic/setup.bash
 
