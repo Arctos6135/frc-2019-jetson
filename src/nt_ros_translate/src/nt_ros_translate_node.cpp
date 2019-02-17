f520596f126a806d1cdfa59b90fe3200cb59e691
Tyler Tian <tylertian123@gmail.com>
2019-02-17 17:51:43 -0500

    Add shutdown entry reset before shutdown

--- src/nt_ros_translate/src/nt_ros_translate_node.cpp
+++ src/nt_ros_translate/src/nt_ros_translate_node.cpp
@@ -57,6 +57,7 @@ class VisionTableListener : public ITableListener {
 		}
         // Jetson graceful shutdown
         else if(key.compare("shutdown") == 0 && value->IsBoolean() && value->GetBoolean()) {
+            table->PutBoolean("shutdown", false);
             std::system("shutdown -P now");
         }
         else if(key.compare("restart-server") == 0 && value->isBoolean() && value->GetBoolean()) {
