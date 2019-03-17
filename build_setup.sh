755109c39275ab92cac59a3b904ac72612c928af
Tyler Tian <tylertian123@gmail.com>
2019-03-17 17:26:05 -0400

    Fix setup files

--- build_setup.sh
+++ build_setup.sh
@@ -8,4 +8,5 @@ sudo apt install ros-kinetic-camera-info-manager ros-kinetic-web-video-server
 rosdep install -y --from-paths .
 wstool update
 source /opt/ros/kinetic/setup.bash
+sudo chown -R "$USER":"$USER"
 
--- build_setup_melodic.sh
+++ build_setup_melodic.sh
@@ -1,11 +1,10 @@
 catkin init
 cd src
 export ROS_DISTRO=melodic
-sudo wstool update
-#rosinstall_generator usb_cam --rosdistro melodic | wstool merge -
-#wstool update
+wstool update
 sudo apt install ros-melodic-camera-info-manager ros-melodic-web-video-server
 rosdep install -y --from-paths .
-sudo wstool update
+wstool update
 source /opt/ros/melodic/setup.bash
+sudo chown -R "$USER":"$USER"
 
