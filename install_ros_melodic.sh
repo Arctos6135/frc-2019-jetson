60fe9921e098425c2ea7f1ea3ae9bb9a61b0d6fc
Tyler Tian <tylertian123@gmail.com>
2019-03-16 20:38:05 -0400

    Add failing ROS melodic setup scripts

---
+++ install_ros_melodic.sh
@@ -0,0 +1,12 @@
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update
+sudo apt-get install ros-melodic-desktop
+sudo rosdep init
+rosdep update
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get install python-catkin-tools
+sudo apt install python-rosinstall-generator
+
