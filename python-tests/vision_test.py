19c8c0debf500c6fdfdc0efcecb78a0bb3ae525a
Tyler Tian <tylertian123@gmail.com>
2019-09-04 23:46:21 -0400

    Improve vision_test.py
    Add the math that calculates the exact position of the target as well as
    its angle offset to vision_test.py.

--- python-tests/vision_test.py
+++ python-tests/vision_test.py
@@ -97,6 +97,61 @@ def is_valid_pair(rects, all_rects):
     else:
         return False
 
+def get_advanced_info(rects):
+    # Calculate the distances to each piece of tape from their y coordinates
+    dist1 = get_rect_distance(rects[0])
+    dist2 = get_rect_distance(rects[1])
+
+    # Calculate the angle difference
+    x_angle1 = get_horiz_angle(rects[0][0][0])
+    x_angle2 = get_horiz_angle(rects[1][0][0])
+    diff = max(x_angle1, x_angle2) - min(x_angle1, x_angle2)
+    # Use the cosine law to find an estimate for the space between the two tapes
+    estimated_spacing = sqrt(dist1 * dist1 + dist2 * dist2 - 2 * dist1 * dist2 * cos(diff))
+    # Divide the actual by the estimate to get an error multiplier
+    error_multiplier = tape_gap / estimated_spacing
+    # Multiply both distances by the error multiplier to improve our estimate
+    dist1 *= error_multiplier
+    dist2 *= error_multiplier
+
+    # Find out the relation of dist1 and dist2, as well as which angle is the one on the left
+    left_side = 0
+    right_side = 0
+    left_angle = 0
+    right_angle = 0
+    
+    # Tape 1 is on the left of tape 2
+    if x_angle1 < x_angle2:
+        left_side = dist1
+        right_side = dist2
+        left_angle = x_angle1
+        right_angle = x_angle2
+    # Tape 1 is on the right of tape 2
+    else:
+        left_side = dist2
+        right_side = dist1
+        left_angle = x_angle2
+        right_angle = x_angle1
+    
+    # Calculate the angle between the line normal to the target plane and the line normal to the camera plane
+    # Use the cosine law again to get the angle between the left side and the target plane
+    theta = acos((left_side * left_side + tape_gap * tape_gap - right_side * right_side) / (2 * left_side * tape_gap))
+    angle_offset = (pi / 2 - theta) + left_angle
+
+    # Calculate the coordinates of the centre of the target
+    # Add the pi/2 since our angles start at the y axis and not the x axis
+    # Negate the angles since they go cw instead of ccw
+    x1 = cos(-left_angle + pi / 2) * left_side
+    y1 = sin(-left_angle + pi / 2) * left_side
+    x2 = cos(-right_angle + pi / 2) * right_side
+    y2 = sin(-right_angle + pi / 2) * right_side
+    # Take the midpoint as the overall position
+    target_x = (x1 + x2) / 2
+    target_y = (y1 + y2) / 2
+
+    # Return a tuple of the position and the offset
+    return ((target_x, target_y), angle_offset)
+
 def draw_rect(img, rect, color=(255, 0, 0), thickness=2):
     box = np.int0(cv2.boxPoints(rect))
     cv2.drawContours(img, [box], 0, color, thickness)
@@ -105,8 +160,9 @@ def draw_rect_of_rects(img, rect0, rect1, color=(255, 0, 0), thickness=4):
     points = np.append(cv2.boxPoints(rect0), cv2.boxPoints(rect1), axis=0)
     x, y, w, h = cv2.boundingRect(points)
     cv2.rectangle(img, (x,y), (x+w,y+h), color, thickness)
-    d = (get_rect_distance(rect0) + get_rect_distance(rect1)) / 2
-    cv2.putText(img, "Distance: {0:.3f}".format(d), (x, y - 6), cv2.FONT_HERSHEY_DUPLEX, 2, color)
+    position, offset = get_advanced_info((rect0, rect1))
+    cv2.putText(img, "Angle Offset: {0:.5f}".format(offset), (x, y - 36), cv2.FONT_HERSHEY_DUPLEX, 1, color)
+    cv2.putText(img, "Position: ({0:.3f}, {1:.3f})".format(position[0], position[1]), (x, y - 6), cv2.FONT_HERSHEY_DUPLEX, 1, color)
 
 colors = [
     (0, 0, 0xFF),
